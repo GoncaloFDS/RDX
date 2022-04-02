62609e3b444059665ba631da555d1b20d5aa6f6c
Gonçalo Soares <goncalofds@gmail.com>
2022-04-02 17:00:59 +0200

    Vulkan 1.3 fresh start

--- build.rs
+++ build.rs
@@ -4,22 +4,6 @@ use spirv_builder::{Capability, MetadataPrintout, SpirvBuilder};
 
 fn main() -> Result<(), Box<dyn Error>> {
     SpirvBuilder::new("./shaders/raster", "spirv-unknown-vulkan1.2")
-        .capability(Capability::RayTracingKHR)
-        .extension("SPV_KHR_ray_tracing")
-        .print_metadata(MetadataPrintout::Full)
-        .build()?;
-
-    SpirvBuilder::new("./shaders/raytracing", "spirv-unknown-spv1.3")
-        .capability(Capability::RayTracingKHR)
-        .capability(Capability::RuntimeDescriptorArray)
-        .extension("SPV_KHR_ray_tracing")
-        .extension("SPV_EXT_descriptor_indexing")
-        .print_metadata(MetadataPrintout::Full)
-        .build()?;
-
-    SpirvBuilder::new("./shaders/ui", "spirv-unknown-vulkan1.2")
-        .capability(Capability::RayTracingKHR)
-        .extension("SPV_KHR_ray_tracing")
         .print_metadata(MetadataPrintout::Full)
         .build()?;
 
--- shaders/raster/src/lib.rs
+++ shaders/raster/src/lib.rs
@@ -10,22 +10,23 @@ use spirv_std::macros::spirv;
 
 use spirv_std::glam::{uvec2, vec2, vec3, vec4, Mat4, UVec3, Vec2, Vec3, Vec4};
 
-pub struct UBO {
-    pub view_model: Mat4,
-    pub projection: Mat4,
-}
-
 #[spirv(vertex)]
 pub fn main_vs(
     #[spirv(vertex_index)] vert_id: i32,
     #[spirv(position)] out_pos: &mut Vec4,
-    #[spirv(uniform, descriptor_set = 0, binding = 0)] ubo: &UBO,
-    position: Vec3,
+    v_color: &mut Vec3,
 ) {
-    *out_pos = ubo.projection * ubo.view_model * position.extend(1.0);
+    let positions: [Vec2; 3] = [vec2(0.0, -0.5), vec2(0.5, 0.5), vec2(-0.5, 0.5)];
+    let colors: [Vec3; 3] = [
+        vec3(1.0, 0.0, 0.0),
+        vec3(0.0, 1.0, 0.0),
+        vec3(0.0, 0.0, 1.0),
+    ];
+    *out_pos = positions[vert_id as usize].extend(0.0).extend(1.0);
+    *v_color = colors[vert_id as usize];
 }
 
 #[spirv(fragment)]
-pub fn main_fs(output: &mut Vec4, #[spirv(uniform, descriptor_set = 0, binding = 0)] ubo: &UBO) {
-    *output = vec4(0.2, 0.4, 0.2, 1.0);
+pub fn main_fs(output: &mut Vec4, v_color: Vec3) {
+    *output = v_color.extend(1.0);
 }
